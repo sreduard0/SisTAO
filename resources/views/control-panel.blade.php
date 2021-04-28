64b1752b95f157deb69d2b56fff59f8ef7d03333
3° Batalhão de Suprimento - Dev <3bsup-dev@gmail.com>
2021-04-28 16:15:17 -0300

    painel de controle

--- resources/views/control-panel.blade.php
+++ resources/views/control-panel.blade.php
@@ -112,25 +112,103 @@
     <section class="content">
       <div class="container-fluid">
         <!-- Small boxes (Stat box) -->
+
+
         <div class="row">
 
           <!-- ./col -->
-          <div class="col-lg-3 col-6">
+        <!-- box -->  <div class="col-lg-3 col-6">
+            <a href="#" class="small-box bg-success">
+                <div class="inner">
+                  <h3>SPED</h3>
+                  <p>Adm</p>
+                </div>
+                {{-- <div class="icon">
+                  <i class="ion ion-stats-bars"></i>
+                </div> --}}
+              </a>
 
-            <!-- box sped -->
-            <div class="small-box bg-success">
-              <div class="inner">
-                <h3>SPED</h3>
-                <p>Adm</p>
-              </div>
-              {{-- <div class="icon">
-                <i class="ion ion-stats-bars"></i>
-              </div> --}}
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-            {{-- box sped --}}
+             </div>{{-- box  --}}
 
-          </div>
+              <!-- box -->  <div class="col-lg-3 col-6">
+            <a href="#" class="small-box bg-success">
+                <div class="inner">
+                  <h3>SPED</h3>
+                  <p>Adm</p>
+                </div>
+                {{-- <div class="icon">
+                  <i class="ion ion-stats-bars"></i>
+                </div> --}}
+              </a>
+
+             </div>{{-- box  --}}
+
+              <!-- box -->  <div class="col-lg-3 col-6">
+            <a href="#" class="small-box bg-success">
+                <div class="inner">
+                  <h3>SPED</h3>
+                  <p>Adm</p>
+                </div>
+                {{-- <div class="icon">
+                  <i class="ion ion-stats-bars"></i>
+                </div> --}}
+              </a>
+
+             </div>{{-- box  --}}
+
+              <!-- box -->  <div class="col-lg-3 col-6">
+            <a href="#" class="small-box bg-success">
+                <div class="inner">
+                  <h3>SPED</h3>
+                  <p>Adm</p>
+                </div>
+                {{-- <div class="icon">
+                  <i class="ion ion-stats-bars"></i>
+                </div> --}}
+              </a>
+
+             </div>{{-- box  --}}
+
+              <!-- box -->  <div class="col-lg-3 col-6">
+            <a href="#" class="small-box bg-success">
+                <div class="inner">
+                  <h3>SPED</h3>
+                  <p>Adm</p>
+                </div>
+                {{-- <div class="icon">
+                  <i class="ion ion-stats-bars"></i>
+                </div> --}}
+              </a>
+
+             </div>{{-- box  --}}
+
+              <!-- box -->  <div class="col-lg-3 col-6">
+            <a href="#" class="small-box bg-success">
+                <div class="inner">
+                  <h3>SPED</h3>
+                  <p>Adm</p>
+                </div>
+                {{-- <div class="icon">
+                  <i class="ion ion-stats-bars"></i>
+                </div> --}}
+              </a>
+
+             </div>{{-- box  --}}
+
+              <!-- box -->  <div class="col-lg-3 col-6">
+            <a href="#" class="small-box bg-success">
+                <div class="inner">
+                  <h3>SPED</h3>
+                  <p>Adm</p>
+                </div>
+                {{-- <div class="icon">
+                  <i class="ion ion-stats-bars"></i>
+                </div> --}}
+              </a>
+
+             </div>{{-- box  --}}
+
+
 
       </div>
     </section>
